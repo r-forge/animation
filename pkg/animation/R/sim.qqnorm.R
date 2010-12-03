d14a6ee70870a53dc8bb11ebd157e8836d1b7ed7
yihui (Yihui Xie) <xie@yihui.name>
2010-12-03 02:47:15 +0000

    the packages will no longer be maintained here; please go to http://github.com/yihui
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/animation@168 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/animation/R/sim.qqnorm.R
+++
@@ -1,8 +0,0 @@
-sim.qqnorm <-
-function(n = 20, ...) {
-    for(i in 1:ani.options("nmax")) {
-        qqnorm(rnorm(n),...)
-        Sys.sleep(ani.options("interval"))
-    }
-}
-
--- pkg/animation/R/zzz.R
+++
@@ -1,6 +0,0 @@
-.First.lib <- function(lib, pkg) {
-    options(demo.ask = FALSE, example.ask = FALSE)
-}
-.Last.lib <- function(lib) {
-    options(ani = NULL)
-}
